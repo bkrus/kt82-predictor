7c95bd68bf20655c7a9717fa45fb2fd211cba4f5
Robert Krussel <bkrus17@gmail.com>
2026-05-15 15:05:00 -0500

    adds strava connect button

--- api/auth/strava/callback.js
+++ api/auth/strava/callback.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.VITE_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
